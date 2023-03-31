e3bca7207f8f0931566799b0196edb8e0e65825f
dat3 <phantiendat14012002@gmail.com>
2023-03-31 20:08:15 +0700

    .

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({price, title, tags}) => (
+  <div className='app__menuitem'>
+      <div className='app__menuitem-head'>
+        <div className='app__menuitem-name'>
+          <p className="p__cormorant">{title}</p>
+        </div>
+        <div className='app__menuitem-price'>
+          <p className="p__cormorant">{price}</p>
+        </div>
+      </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-
+import { SubHeading, MenuItem } from '../../components'
+import {data, images} from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu section__padding flex__center'>
+    <div className='app__specialMenu-title'>
+        <SubHeading title='Menu That Fits Your Palatte' />
+        <h3 className='headtext__cormorant'>Today's Special</h3>
+    </div>
+    <div className='app__specialMenu-menu'>
+      <div className='app__specialMenu-menu_wines flex__center'>
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+        <div className='app__specialMenu-menu_items'>
+          {data.wines.map((wine, index) => (
+            <MenuItem key={wine.title + index} title={wine.title} tags={wine.tags} price={wine.price}/>
+          ))}
+        </div>
+      </div>
+      <div className='app__specialMenu-menu_img'>
+        <img src={images.menu} alt="" />
+      </div>
+
+      <div className='app__specialMenu-menu_cocktails flex__center'>
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
+        <div className='app__specialMenu-menu_items'>
+          {data.cocktails.map((cocktail, index) => (
+            <MenuItem key={cocktail.title + index} title={cocktail.title} tags={cocktail.tags} price={cocktail.price}/>
+          ))}
+        </div>
+      </div>
+    </div>
   </div>
 );
 
